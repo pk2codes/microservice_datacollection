69e6ae0804b46c7e8bdd4d56ee7434821343c1b5
Patrick.Welter <patrick.welter@janitza.de>
2017-03-22 15:00:40 +0100

    persisting incoming online recordings

--- data-store-service/src/main/java/org/thm/datacollector/infrastructure/stream/IValueDataReceiver.java
+++ data-store-service/src/main/java/org/thm/datacollector/infrastructure/stream/IValueDataReceiver.java
@@ -1,13 +1,18 @@
 package org.thm.datacollector.infrastructure.stream;
 
+import com.netflix.discovery.converters.Auto;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.thm.datacollector.domain.model.DataStoreIValInfo;
+import org.thm.datacollector.infrastructure.persistence.DataStoreIValRepository;
 import org.thm.datacollector.infrastructure.rest.Client;
 import org.thm.datacollector.infrastructure.stream.channel.DataIValueSink;
 import org.thm.datacollector.infrastructure.stream.channel.DataResourceSink;
 import org.thm.datacollector.model.OnlineRecording;
+
+import java.util.Optional;
 
 /**
  * Created by patrick.welter on 3/6/17.
@@ -22,9 +27,23 @@ public class IValueDataReceiver {
     @Autowired
     private Client.SaveRecording saveRecording;
 
+    @Autowired
+    private DataStoreIValRepository repo;
+
     @ServiceActivator(inputChannel = DataIValueSink.INPUT)
     public void dataReceived(final OnlineRecording rec) {
         log.info("received:" + rec.toString());
-        //saveRecording.dataLocation(rec);
+        final Optional<DataStoreIValInfo> optInfo =
+                repo.findFirstBy("ival", rec.getType());
+        if(optInfo.isPresent()) {
+            final DataStoreIValInfo info = optInfo.get();
+            final boolean isStorable = info.getIval().equals(rec.getType()) && info.isStored();
+            if(isStorable){
+                log.info("persisting data: "+ rec.toString());
+                saveRecording.dataLocation(rec);
+            }
+        } else {
+            log.info("no store info found for:" + rec.toString());
+        }
     }
 }
